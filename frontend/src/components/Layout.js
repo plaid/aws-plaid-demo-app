6507b7d9a70c55ba2830b9db0183d8c8c48f80d2
jacobdgoren <134474294+jacobdgoren@users.noreply.github.com>
2023-05-24 10:19:26 -0700

    Rename to monjdg

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -24,7 +24,7 @@ export default function Layout() {
           <Button onClick={() => logOut()}>Logout</Button>
         )}
       </nav>
-      <Heading level={2}>Vista Finance</Heading>
+      <Heading level={2}>MonJDG Finance</Heading>
       <View>
         {route === 'authenticated' ? `Welcome ${user.signInUserSession.idToken.payload.email}` : 'Please Login!'}
       </View>
