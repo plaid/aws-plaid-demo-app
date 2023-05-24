528292c30b6231816ae94102e38814a6b5b6f7f6
jacobdgoren <134474294+jacobdgoren@users.noreply.github.com>
2023-05-24 09:29:16 -0700

    mandatorySignIn true

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { Amplify, Auth } from "aws-amplify";
+import { Amplify, Auth, API } from "aws-amplify";
 import App from './App';
 
 import '@aws-amplify/ui-react/styles.css';
@@ -19,7 +19,7 @@ Amplify.configure({
     region: process.env.REACT_APP_REGION,
     userPoolId: process.env.REACT_APP_COGNTIO_USERPOOL_ID,
     userPoolWebClientId: process.env.REACT_APP_COGNITO_CLIENT_ID,
-    mandatorySignIn: false,
+    mandatorySignIn: true,
     oauth: {
       domain: process.env.REACT_APP_COGNITO_DOMAIN,
       scope: ['email', 'openid', `${process.env.REACT_APP_BACKEND_URL}/plaid.rw}`],
@@ -49,7 +49,37 @@ Auth.configure({
     region: process.env.REACT_APP_REGION,
     userPoolId: process.env.REACT_APP_COGNTIO_USERPOOL_ID,
     userPoolWebClientId: process.env.REACT_APP_COGNITO_CLIENT_ID,
-    mandatorySignIn: false,
+    mandatorySignIn: true,
+    oauth: {
+      domain: process.env.REACT_APP_COGNITO_DOMAIN,
+      scope: ['email', 'openid', `${process.env.REACT_APP_BACKEND_URL}/plaid.rw}`],
+      responseType: 'code'
+    }
+  },
+  API: {
+    endpoints: [
+      {
+        name: "plaidapi",
+        endpoint: process.env.REACT_APP_BACKEND_URL,
+        region: process.env.REACT_APP_REGION,
+        clientId: process.env.REACT_APP_COGNITO_CLIENT_ID,
+        custom_header: async () => {
+          return { Authorization: `Bearer ${(await Auth.currentSession()).getAccessToken().getJwtToken()}` }
+        }
+      }
+    ]
+  }
+});
+
+API.configure({
+  aws_appsync_graphqlEndpoint: process.env.REACT_APP_GRAPHQL_URL,
+  aws_appsync_region: process.env.REACT_APP_REGION,
+  aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS',
+  Auth: {
+    region: process.env.REACT_APP_REGION,
+    userPoolId: process.env.REACT_APP_COGNTIO_USERPOOL_ID,
+    userPoolWebClientId: process.env.REACT_APP_COGNITO_CLIENT_ID,
+    mandatorySignIn: true,
     oauth: {
       domain: process.env.REACT_APP_COGNITO_DOMAIN,
       scope: ['email', 'openid', `${process.env.REACT_APP_BACKEND_URL}/plaid.rw}`],
