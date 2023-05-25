d1884245eb54c8dc89623db10246c9366b517889
jacobdgoren <134474294+jacobdgoren@users.noreply.github.com>
2023-05-24 22:25:19 -0700

    amplifyConfig={authConf}

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -3,8 +3,6 @@ import { useAuthenticator, View } from '@aws-amplify/ui-react';
 import { useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router';
 
-import { Amplify, Auth, Hub } from 'aws-amplify';
-import { CognitoHostedUIIdentityProvider } from '@aws-amplify/auth';
 
 export default function Login() {
   const { route } = useAuthenticator((context) => [context.route]);
@@ -12,12 +10,46 @@ export default function Login() {
   const navigate = useNavigate();
   let from = location.state?.from?.pathname || '/';
 
+  
+
+  const federated = { googleClientId: "705034194853-kht77urbsc6l7evgs7khk48h7lej835f.apps.googleusercontent.com"}
+
+  const authConf =
+  {
+    aws_appsync_graphqlEndpoint: process.env.REACT_APP_GRAPHQL_URL,
+    aws_appsync_region: process.env.REACT_APP_REGION,
+    aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS',
+    Auth: {
+      region: process.env.REACT_APP_REGION,
+      userPoolId: process.env.REACT_APP_COGNTIO_USERPOOL_ID,
+      userPoolWebClientId: process.env.REACT_APP_COGNITO_CLIENT_ID,
+      mandatorySignIn: true,
+      oauth: {
+        domain: process.env.REACT_APP_COGNITO_DOMAIN,
+        scope: ['email', 'openid', `${process.env.REACT_APP_BACKEND_URL}/plaid.rw}`],
+        responseType: 'code'
+      }
+    },
+    API: {
+      endpoints: [
+        {
+          name: "plaidapi",
+          endpoint: process.env.REACT_APP_BACKEND_URL,
+          region: process.env.REACT_APP_REGION,
+          clientId: process.env.REACT_APP_COGNITO_CLIENT_ID,
+          custom_header: async () => {
+            return { Authorization: `Bearer ${(await Auth.currentSession()).getAccessToken().getJwtToken()}` }
+          }
+        }
+      ]
+    }
+  }
+
   const components = {
     SignUp: {
       Footer() {
         return (
           <View textAlign="center">
-            <button onClick={() => Auth.federatedSignIn({provider: CognitoHostedUIIdentityProvider.Google })}>Open Google</button>
             <strong>Password Policy</strong>:
             <ul>
               <li>Minimum of 8 characters</li>
@@ -40,7 +72,7 @@ export default function Login() {
 
   return (
     <View className="auth-wrapper">
-      <Authenticator components={components} socialProviders={['google']}/>
+      <Authenticator components={components} federated={federated} amplifyConfig={authConf} socialProviders={['google']}/>
     </View>
   );
 }
