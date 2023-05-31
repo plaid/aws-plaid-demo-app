a8e95c97e6d07aa7af62557f2d09e2c3bac45c23
Jacob Goren <jacobgoren@Jacobs-MacBook-Pro.local>
2023-05-30 22:06:55 -0700

    x

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 
 
 import { createRoot } from 'react-dom/client';
-import { Amplify, Auth, API } from "aws-amplify";
+import { Amplify, Auth } from "aws-amplify";
 import App from './App';
 
 import '@aws-amplify/ui-react/styles.css';
@@ -27,8 +27,8 @@ Amplify.configure({
       domain: process.env.REACT_APP_COGNITO_DOMAIN,
       scope: ['openid', 'email', 'profile', 'aws.cognito.signin.user.admin'],
       responseType: 'code',
-      redirectSignIn: 'https://monjdg.com',
-      redirectSignOut: 'https://monjdg.com',
+      redirectSignIn: 'http://localhost:3000',
+      redirectSignOut: 'http://localhost:3000',
     }
   },
   API: {
@@ -46,68 +46,5 @@ Amplify.configure({
   }
 });
 
-Auth.configure({
-  aws_appsync_graphqlEndpoint: process.env.REACT_APP_GRAPHQL_URL,
-  aws_appsync_region: process.env.REACT_APP_REGION,
-  aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS',
-  Auth: {
-    region: process.env.REACT_APP_REGION,
-    userPoolId: process.env.REACT_APP_COGNTIO_USERPOOL_ID,
-    userPoolWebClientId: process.env.REACT_APP_COGNITO_CLIENT_ID,
-    mandatorySignIn: true,
-    oauth: {
-      domain: process.env.REACT_APP_COGNITO_DOMAIN,
-      scope: ['openid', 'email', 'profile', 'aws.cognito.signin.user.admin'],
-      responseType: 'code',
-      redirectSignIn: 'https://monjdg.com',
-      redirectSignOut: 'https://monjdg.com',
-    }
-  },
-  API: {
-    endpoints: [
-      {
-        name: "plaidapi",
-        endpoint: process.env.REACT_APP_BACKEND_URL,
-        region: process.env.REACT_APP_REGION,
-        clientId: process.env.REACT_APP_COGNITO_CLIENT_ID,
-        custom_header: async () => {
-          return { Authorization: `Bearer ${(await Auth.currentSession()).getAccessToken().getJwtToken()}` }
-        }
-      }
-    ]
-  }
-});
-
-API.configure({
-  aws_appsync_graphqlEndpoint: process.env.REACT_APP_GRAPHQL_URL,
-  aws_appsync_region: process.env.REACT_APP_REGION,
-  aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS',
-  Auth: {
-    region: process.env.REACT_APP_REGION,
-    userPoolId: process.env.REACT_APP_COGNTIO_USERPOOL_ID,
-    userPoolWebClientId: process.env.REACT_APP_COGNITO_CLIENT_ID,
-    mandatorySignIn: true,
-    oauth: {
-      domain: process.env.REACT_APP_COGNITO_DOMAIN,
-      scope: ['openid', 'email', 'profile', 'aws.cognito.signin.user.admin'],
-      responseType: 'code',
-      redirectSignIn: 'https://monjdg.com',
-      redirectSignOut: 'https://monjdg.com',
-    }
-  },
-  API: {
-    endpoints: [
-      {
-        name: "plaidapi",
-        endpoint: process.env.REACT_APP_BACKEND_URL,
-        region: process.env.REACT_APP_REGION,
-        clientId: process.env.REACT_APP_COGNITO_CLIENT_ID,
-        custom_header: async () => {
-          return { Authorization: `Bearer ${(await Auth.currentSession()).getAccessToken().getJwtToken()}` }
-        }
-      }
-    ]
-  }
-});
 
 root.render(<App />);
